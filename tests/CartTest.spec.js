c8af7938817a9e581270a783a951a8d7761ff62f
Heitor <heitor-seemann@outlook.com>
2026-07-07 15:39:34 -0300

    api person

--- tests/CartTest.spec.js
+++ tests/CartTest.spec.js
@@ -1,20 +1,17 @@
 const { test, expect } = require('@playwright/test');
-const LoginPage = require('../pages/LoginPage');
 const CartPage = require('../pages/CartPage');
 
-let loginPage, cartPage;
+let cartPage;
 
 test.beforeEach(async ({ page }) => {
-  loginPage = new LoginPage(page);
   cartPage = new CartPage(page);
 });
 
-
 test('Select a product and add it to the cart on Automation Exercise', async ({ page }) => {
-  await loginPage.login();
+  await page.goto('/'); 
   await cartPage.addToCart();
   await cartPage.checkTheProductAdded();
-  await loginPage.login();
+  await page.goto('/'); 
   await cartPage.accessTheCart();
   await cartPage.deleteTheCart();
 });
